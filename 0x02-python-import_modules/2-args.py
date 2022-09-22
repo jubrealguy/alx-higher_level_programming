52511c76401171db8d00ba9748834b982f562ad7
Jubreel Adebayo <whizkidjubrealguy@gmail.com>
2022-09-22 04:42:04 -0700

    print arguments and import from sys

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
 
-if len(argv) == 1:
-    print("{:d} {:s}".format(0, "arguments."))
+    if len(argv) == 1:
+        print("{:d} {:s}".format(0, "arguments."))
 
-elif len(argv) == 2:
-    print("{:d} {:s}".format(1, "argument:"))
-    print("{:d}: {:s}".format(1, argv[1]))
+    elif len(argv) == 2:
+        print("{:d} {:s}".format(1, "argument:"))
+        print("{:d}: {:s}".format(1, argv[1]))
 
-elif len(argv) > 2:
-    print("{:d} {:s}".format(len(argv) - 1, "arguments:"))
+    elif len(argv) > 2:
+        print("{:d} {:s}".format(len(argv) - 1, "arguments:"))
 
-    for i in range(1, len(argv)):
-        print("{:d}: {:s}".format(i, argv[i]))
+        for i in range(1, len(argv)):
+            print("{:d}: {:s}".format(i, argv[i]))
