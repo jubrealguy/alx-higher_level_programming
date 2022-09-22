bdf013dd510e5314959832cc849c5d507d9ae620
Jubreel Adebayo <whizkidjubrealguy@gmail.com>
2022-09-22 04:58:44 -0700

    adding integer arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    sum = 0
+
+    if len(argv) == 1:
+        sum = 0
+    
+    elif len(argv) == 2:
+        sum += int(argv[1])
+
+    elif len(argv) > 2:
+        for i in range(1, len(argv)):
+            sum += int(argv[i])
+
+    print(sum)
