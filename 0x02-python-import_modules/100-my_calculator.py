5194960a2cdca73a53e27c18f431146cababc2f6
Jubreel Adebayo <whizkidjubrealguy@gmail.com>
2022-09-22 07:54:02 -0700

    bug fixed, ',' instead of '.' in sub function

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-from sys import argv
-from calculator_1 import add, sub, mul, div
-
 if __name__ == "__main__":
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
+
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
@@ -16,7 +16,7 @@ if __name__ == "__main__":
             print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 
         elif argv[2] == op[1]:
-            print("{:d} - {:d} = {:d}".format(a, b. sub(a, b)))
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
 
         elif argv[2] == op[2]:
             print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
