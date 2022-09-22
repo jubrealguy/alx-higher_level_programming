695f419dd34f449f2f95a27c26128cab2ae504f2
Jubreel Adebayo <whizkidjubrealguy@gmail.com>
2022-09-22 06:10:52 -0700

    calculator for arguments on the terminal

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
-    from calculator_1 import add, sub, mul, div
+from sys import argv
+from calculator_1 import add, sub, mul, div
+
+if __name == "__main__":
 
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
