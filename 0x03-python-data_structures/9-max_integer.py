c82f575a5c6e418b86a017141cda524a53b72b4b
Jubreel Adebayo <whizkidjubrealguy@gmail.com>
2022-09-25 13:24:33 -0700

    checking for maximum number in an array

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    maxi = 0
-
     if not my_list:
         return None
     else:
+        maxi = my_list[0]
         for i in my_list:
             if i > maxi:
                 maxi = i
