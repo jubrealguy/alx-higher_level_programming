456322d73052bb946a60860559622cfe5d7093b2
Jubreel Adebayo <whizkidjubrealguy@gmail.com>
2022-09-28 14:35:17 -0700

    replace an item

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,6 +2,6 @@
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
     for i in range(len(new_list)):
-        if my_list[i] == search:
-            my_list[i] = replace
+        if new_list[i] == search:
+            new_list[i] = replace
     return new_list
