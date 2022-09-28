30ba291c4360a800f74c6de6fb440d878caad2bb
Jubreel Adebayo <whizkidjubrealguy@gmail.com>
2022-09-28 16:34:58 -0700

    key for highest value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,4 +3,9 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     else:
-        return(max(a_dictionary, key=a_dictionary.get))
+        biggest = list(a_dictionary.keys())[0]
+
+        for item in a_dictionary:
+            if a_dictionary[item] > a_dictionary[biggest]:
+                biggest = item
+        return biggest
