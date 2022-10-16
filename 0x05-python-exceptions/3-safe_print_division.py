74f59cceec0302c90d8d463c7ad9504c3d1484ba
Jubreel Adebayo <whizkidjubrealguy@gmail.com>
2022-10-16 13:15:24 -0700

    making None a keyword and not a string

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,6 +3,6 @@ def safe_print_division(a, b):
     try:
         div = a/ b
     except ZeroDivisionError:
-        div = "None"
+        div = None
     finally:
         print("Inside result: {}".format(div))
