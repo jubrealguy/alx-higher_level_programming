b5d0421ecc78cf8bfe4ca1e7c7ecb66271857ced
Jubreel Adebayo <whizkidjubrealguy@gmail.com>
2022-10-16 13:14:02 -0700

    using removed the print function on try and except

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,9 +2,7 @@
 def safe_print_division(a, b):
     try:
         div = a/ b
-        print("{} / {} = {}".format(a, b, div))
     except ZeroDivisionError:
         div = "None"
-        print("{} / {} = {}".format(a, b, div))
     finally:
         print("Inside result: {}".format(div))
