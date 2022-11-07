49c747955610f16d4afbb9a8aebef0013b6a8586
Jubreel Adebayo <whizkidjubrealguy@gmail.com>
2022-11-07 04:10:14 -0800

    removed the area method

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,7 +10,3 @@ class Square:
             raise TypeError("size must be an integer")
         if self.__size < 0:
             raise ValueError("size must be >= 0")
-
-    """Return the square"""
-    def area(self):
-        return self.__size * self.__size
