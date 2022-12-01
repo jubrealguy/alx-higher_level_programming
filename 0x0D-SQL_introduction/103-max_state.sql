6eedeca4ca8d5cee7be8caa1d1841af3feca62d6
Jubreel Adebayo <whizkidjubrealguy@gmail.com>
2022-12-01 05:59:30 -0800

    temperatures not temperature

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,2 +1,2 @@
 -- import hbtn_0c_0 and display the max temperature of each state
-SELECT state, MAX(value) as max_temp from temperature group by state order by state ASC LIMIT 3
+SELECT state, MAX(value) as max_temp from temperatures group by state order by state ASC LIMIT 3
