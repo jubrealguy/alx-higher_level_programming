8a440aeb56ae80807c12afa97c901adab639a739
Jubreel Adebayo <whizkidjubrealguy@gmail.com>
2022-12-03 06:28:21 -0800

    selecting just id and name

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- A  script that lists all the cities of California
 -- that can be found in the database hbtn_0d_usa
-SELECT * FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') GROUP BY id ORDER BY id ASC;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') GROUP BY id ORDER BY id ASC;
