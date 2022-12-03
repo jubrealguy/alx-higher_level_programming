2dfbc4ddc5e6b0b655373ea295789c0f7bb37563
Jubreel Adebayo <whizkidjubrealguy@gmail.com>
2022-12-03 06:26:07 -0800

    changed id to state_id

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- A  script that lists all the cities of California
 -- that can be found in the database hbtn_0d_usa
-SELECT * FROM cities WHERE id = (SELECT id FROM states WHERE name = 'California') GROUP BY id ORDER BY id ASC;
+SELECT * FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') GROUP BY id ORDER BY id ASC;
