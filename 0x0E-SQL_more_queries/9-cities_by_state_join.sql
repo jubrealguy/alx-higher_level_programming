49dfeffc108fe9ff7dd47482fe7665d02dae9bfa
Jubreel Adebayo <whizkidjubrealguy@gmail.com>
2022-12-03 06:40:29 -0800

    join not natural join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- A script that lists all cities contained in the database hbtn_0d_usa
 SELECT cities.id, cities.name, states.name FROM cities
-NATURAL JOIN states WHERE cities.state_id = states.id
+JOIN states WHERE cities.state_id = states.id
 ORDER BY cities.id ASC;
