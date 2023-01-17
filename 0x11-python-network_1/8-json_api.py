286cac2e347e1b6359d73f33933bbd429a05c7d8
Adebayo Jubreel <whizkidjubrealguy@gmail.com>
2023-01-17 09:09:24 -0800

    posting second argument as parameter

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+A script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    if len(argv) < 2:
+        letter = ""
+    else:
+        letter = argv[1]
+    url = 'http://0.0.0.0:5000/search_user'
+    values = {'q': letter}
+    req = requests.post(url, data=values)
+
+    try:
+        dic = req.json()
+        if dic:
+            print("[{}] {}".format(dic.get('id'), dic.get('name')))
+        else:
+            print("No result")
+    except ValueError as e:
+        print("Not a valid JSON")
