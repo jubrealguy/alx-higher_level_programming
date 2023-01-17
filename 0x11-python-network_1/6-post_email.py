d257eb22a5f4d3a8dd3abacd7b3e0e1c847374f6
Adebayo Jubreel <whizkidjubrealguy@gmail.com>
2023-01-17 08:51:14 -0800

    send post req to url using requests package

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+A script that takes in a URL and an email address
+sends a POST request to the passed URL
+with the email as a parameter
+and finally displays the body of the response.
+"""
+
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    url = argv[1]
+    values = {'email': argv[2]}
+    req = requests.post(url, data=values)
+    print(req.text)
