7d16a5c2534d479b1e5ebc7df23f1ec1d874f44d
Adebayo Jubreel <whizkidjubrealguy@gmail.com>
2023-01-17 08:28:54 -0800

    error code response

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+A script that takes in a URL, sends a request to the URL
+and displays the body of the response (decoded in utf-8)
+"""
+
+from sys import argv
+import urllib.request
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as err:
+        print("Error code: {}".format(err.code))
