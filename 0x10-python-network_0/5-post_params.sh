b6c0f6514731b321b62f39ac8a88df4bb48131df
Adebayo Jubreel <whizkidjubrealguy@gmail.com>
2023-01-17 16:46:50 -0800

    flag d instead of flag F

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Take in a url POST key and display the response
-curl -s -X POST -F "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
