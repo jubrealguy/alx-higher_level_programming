038bb2099c7f3f2c7993b3d06859075124170c69
Adebayo Jubreel <whizkidjubrealguy@gmail.com>
2023-01-24 06:49:52 -0800

    added coordinates

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+A class square prints squarewith # and position with _
+"""
+
+
+class Square:
+    """ Private instance attributes """
+    def __init__(self, size=0, position=(0,0)):
+        self.__size = size
+        self.__position = position
+
+    """ Retrieving size property """
+    @property
+    def size(self):
+        return self.__size
+
+    """ Retrieving position property """
+    @property
+    def position(self):
+        return self.__position
+
+    """ Updating size property """
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if type(self.__size) != int:
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    """ Updating position property """
+    @position.setter
+    def position(self, value):
+        self.__position = value
+        if len(self.__position) != 2 or self.__position[0] < 0 or self.__position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    """ Returns the area of square """
+    def area(self):
+        return self.__size * self.__size
+
+    """ Prints square with # """
+    def my_print(self):
+        if self.__size > 0:
+            for i in range(self.__size):
+                print("_"*self.__position[0], end='')
+                for j in range(self.__size):
+                    print("#", end='')
+                print()
+        else:
+            print()
