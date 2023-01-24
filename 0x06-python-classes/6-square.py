807a0ae2e051a5ab20ad81e961e6cacfc16ae7e4
Adebayo Jubreel <whizkidjubrealguy@gmail.com>
2023-01-24 13:00:30 -0800

    changed condition of the position setter

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -33,7 +33,7 @@ class Square:
     @position.setter
     def position(self, value):
         self.__position = value
-        if len(self.__position) != 2 or self.__position[0] < 0 or self.__position[1] < 0:
+        if len(self.__position) != 2 or self.__position[0] >= 0 or self.__position[1] >= 0:
             raise TypeError("position must be a tuple of 2 positive integers")
 
     """ Returns the area of square """
