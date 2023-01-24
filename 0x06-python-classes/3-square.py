1e1f30353e67ee01a39090e43667b66395eb927e
Adebayo Jubreel <whizkidjubrealguy@gmail.com>
2023-01-24 04:14:12 -0800

    square returns area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr.bin/python3
+"""
+A class Square with a private instance attribute Size
+and a public instance method Area
+"""
+
+
+class Square:
+    """
+    A private instance attr Size and public instance method area
+    """
+    def __init__(self, size):
+        self.__size = size
+        if type(self.__size) != int:
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size * self.__size
