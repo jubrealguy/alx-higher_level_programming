a8aa1317d90e673d2fc1e9d9e6737c203b23a6df
Adebayo Jubreel <whizkidjubrealguy@gmail.com>
2023-01-24 03:36:39 -0800

    added Readme

--- 0x06-python-classes/0-square.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Create an empty class"""
-
-
-class Square:
-    """execute empty class with PASS"""
-    pass
--- 0x06-python-classes/1-square.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Create a square class with a size value"""
-
-
-class Square:
-    """Create a size value"""
-    def __init__(self, size):
-        self.__size = size
