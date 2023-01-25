390647234035aa3019da96b148bb27980236d3cc
Adebayo Jubreel <whizkidjubrealguy@gmail.com>
2023-01-25 11:01:18 -0800

    final fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,53 +1,89 @@
 #!/usr/bin/python3
 """
-A class square prints squarewith # and position with _
+Module 6-square
+Defines class Square with private size and position; and public area
+Can access and update size and position
+Can print to stdout the square using #'s
 """
 
 
 class Square:
-    """ Private instance attributes """
-    def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+    """
+    class Square definition
+    Args:
+        size (int): size of a side in square
+    Functions:
+        __init__(self, size, position)
+        size(self)
+        size(self, value)
+        position(self)
+        position(self, value)
+        area(self)
+        my_print(self)
+    """
 
-    """ Retrieving size property """
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        Initializes square
+        Attributes:
+            size (int): defaults to 0 if none; don't use __size to call setter
+            position (int): tuple of two positive integers
+        """
+        self.size = size
+        self.position = position
+
     @property
     def size(self):
-        """ Returns size property """
+        """"
+        Getter
+        Return: size
+        """
         return self.__size
 
-    """ Updating size property """
     @size.setter
     def size(self, value):
-        """ updating size to value """
-        self.__size = value
-        if type(self.__size) != int:
+        """
+        Setter
+        Args:
+            value: sets size to value if int and >= 0
+        """
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        elif self.__size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
-    """ Retrieving position property """
     @property
     def position(self):
-        """ Returns position """
+        """"
+        Getter
+        Return: position
+        """
         return self.__position
 
-    """ Updating position property """
     @position.setter
     def position(self, value):
-        """ updating position to value """
-        self.__position = value
-        if len(self.__position) != 2 or type(self.__position) is not tuple or \
-                self.__position[0] < 0 or self.__position[1] < 0 or \
-                type(self.__position[0]) != int or \
-                type(self.__position[1]) != int:
+        """
+        Setter
+        Args:
+            value: sets position to tuple if value is tuple of 2 positive ints
+        """
+        if type(value) is not tuple or len(value) != 2 or \
+           type(value[0]) is not int or type(value[1]) is not int or \
+           value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
 
-    """ Returns the area of square """
     def area(self):
-        return self.__size * self.__size
+        """
+        Calculates area of square
+        Returns:
+            area
+        """
+        return (self.__size)**2
 
-    """ Prints square with # """
     def my_print(self):
         if self.__size == 0:
             print("")
