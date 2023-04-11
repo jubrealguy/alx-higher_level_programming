e18d60ba287f8c9ed653758741e6ce959f46338c
jubrealguy <whizkidjubrealguy@gmail.com>
2023-04-11 17:43:25 +0100

    convert to integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
-const arg = parseInt(process.argv[2], 10);
+const arg = parseInt(process.argv.slice(2)[0], 10);
+
 if (isNaN(arg)) {
   console.log('Not a number');
-} else {
-  console.log(`My number: ${arg}`);
-}
+} else { console.log(`My number: ${arg}`); }
