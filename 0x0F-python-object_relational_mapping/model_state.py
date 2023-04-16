d885ac82795b6c50c579b04776b2beb2ec945036
jubrealguy <whizkidjubrealguy@gmail.com>
2023-04-16 21:31:48 +0100

    A python filr that links to the MYSQL table states

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" A state class that inherits from Base """
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ links to MYSQL table states  """
+    __tablename__ = 'states'
+
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
