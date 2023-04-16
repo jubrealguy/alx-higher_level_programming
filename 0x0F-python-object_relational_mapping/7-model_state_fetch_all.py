43f7a472b9558ad1e6e59d50933bbf2645fbca76
jubrealguy <whizkidjubrealguy@gmail.com>
2023-04-16 22:02:51 +0100

    state objects from database with instances of a python class

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+A script that lists all State objects from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine(
+                "mysql+mysqldb://{}:{}@localhost/{}"
+                .format(argv[1], argv[2], argv[3]), pool_pre_ping=True
+                )
+    Session = sessionmaker(bind=engine)  # Creating a session
+    session = Session()  # Instatiate our Session
+
+    # Database querying using python instances
+    for inst in session.query(State).order_by(State.id):
+        print("{}:{}".format(inst.id, inst.name))
+
+    session.close()
