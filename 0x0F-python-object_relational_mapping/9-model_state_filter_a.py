44cc509d16ad235c7029e3bfb553b4b0cc7bbe94
jubrealguy <whizkidjubrealguy@gmail.com>
2023-04-16 22:45:09 +0100

    checking if a is in state.name

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+A script that prints tate objects from the database hbtn_0e_6_usa
+with the letter a
+"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    # Engine for Database
+    engine = create_engine(
+                "mysql+mysqldb://{}:{}@localhost/{}"
+                .format(argv[1], argv[2], argv[3]), pool_pre_ping=True
+                )
+    Session = sessionmaker(bind=engine)  # Creating a session
+    session = Session()  # Instatiate our Session
+
+    # Database querying using python instances
+    for inst in session.query(State).order_by(State.id):
+        if 'a' in inst.name:
+            print("{:d}: {:s}".format(inst.id, inst.name))
+
+    session.close()
