e4e546d03342e540ad169ba46494e3c3b1b997b6
jubrealguy <whizkidjubrealguy@gmail.com>
2023-04-16 23:10:16 +0100

    adding a new state and commiting into our session

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+A script that lists all State objects from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    # Engine for Database
+    engine = create_engine(
+                "mysql+mysqldb://{}:{}@localhost/{}"
+                .format(argv[1], argv[2], argv[3]), pool_pre_ping=True
+                )
+    Session = sessionmaker(bind=engine)  # Creating a session
+    session = Session()  # Instatiate our Session
+
+    # Adding new state to the table and commit
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+
+    print("{:d}".format(new_state.id))
+
+    session.close()
