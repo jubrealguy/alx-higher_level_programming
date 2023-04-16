c427aa09d9d4af1afe78721f4872515a2c38ad3e
jubrealguy <whizkidjubrealguy@gmail.com>
2023-04-16 23:56:35 +0100

    removed continue

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -10,6 +10,7 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
+
     # Engine for Database
     engine = create_engine(
                 "mysql+mysqldb://{}:{}@localhost/{}"
@@ -18,11 +19,10 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)  # Creating a session
     session = Session()  # Instatiate our Session
 
-    # update the name of state.id 2
+    # deleting states with a in their names
     for inst in session.query(State):
         if 'a' in inst.name:
             session.delete(inst)
-            continue
 
     session.commit()
     session.close()
