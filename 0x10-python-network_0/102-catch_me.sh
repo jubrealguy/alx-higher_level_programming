cd37f563b3e649a37aa70329831109f317c45b76
jubrealguy <whizkidjubrealguy@gmail.com>
2023-04-27 15:59:55 +0100

    server responding with a msg

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,4 @@
 #!/lin/bash
 # A Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
 #!/bin/bash
-curl -s -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" "0.0.0.0:5000/catch_me" -L
+curl -s -X PUT 0.0.0.0:5000/catch_me -d "You got me!"
