973460c74274c2334928849e328e2f284f263e4c
jubrealguy <whizkidjubrealguy@gmail.com>
2023-04-27 14:11:43 +0100

    check for size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # Displays the size of the body of the response
-curl -sI "$1" | grep 'Content-Length:' | cut -f2 -d' '
+res=$(curl -sS "$1")
+size=$(echo -n "$res" | wc -c)
+echo $size
