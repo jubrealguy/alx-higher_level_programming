8dfd2157799ef96be139cd79e0a5db5b83fe051c
jubrealguy <whizkidjubrealguy@gmail.com>
2023-04-28 17:34:25 +0100

    removed text

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,5 +8,5 @@ import requests
 if __name__ == "__main__":
     page = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(page)))
-    print("\t- content: {}".format(page))
+    print("\t- type: {}".format(type(page.text)))
+    print("\t- content: {}".format(page.text))
