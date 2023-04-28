84a3c18ef6a760609dbf89081ef8f6146b38196e
jubrealguy <whizkidjubrealguy@gmail.com>
2023-04-28 18:13:13 +0100

    stored it in a variable

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -11,4 +11,5 @@ from requests.auth import HTTPBasicAuth
 if __name__ == "__main__":
     url = 'https://api.github.com/user'
     req = requests.get(url, auth=HTTPBasicAuth(argv[1], argv[2]))
-    print(req.json().get('id'))
+    display_id = req.json().get('id')
+    print(display_id)
