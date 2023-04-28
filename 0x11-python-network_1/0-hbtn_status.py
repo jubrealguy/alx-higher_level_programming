81a443ab1a9550a0efefdb5090f47bff206292cf
jubrealguy <whizkidjubrealguy@gmail.com>
2023-04-28 16:56:24 +0100

    using urllib to fetch url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,13 +3,12 @@
 fetches https://alx-intranet.hbtn.io/status and display response
 """
 
-import urllib.request
+from urllib.request import urlopen
 
 if __name__ == "__main__":
-    req = urllib.request
-    with req.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        html = response.read()
+    with urlopen('https://alx-intranet.hbtn.io/status') as response:
+        page = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+        print("\t- type: {}".format(type(page)))
+        print("\t- content: {}".format(page))
+        print("\t- utf8 content: {}".format(page.decode('utf-8')))
