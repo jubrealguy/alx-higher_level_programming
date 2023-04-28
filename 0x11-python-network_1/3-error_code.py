3388e9fdaece2da042370d650d1a7d4541479981
jubrealguy <whizkidjubrealguy@gmail.com>
2023-04-28 17:29:42 +0100

    e.code to return the error status code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     try:
         with urllib.request.urlopen(argv[1]) as response:
             print(response.read().decode('utf-8'))
-    except urllib.error.HTTPError as err:
-        print("Error code: {}".format(err.code))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
