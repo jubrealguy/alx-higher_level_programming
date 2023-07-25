11f21d2c78d014032379e523464e158c7cb9d380
jubrealguy <whizkidjubrealguy@gmail.com>
2023-07-25 14:55:23 +0100

    all xters

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,25 +1,41 @@
 #!/usr/bin/node
 const request = require('request');
-const url = process.argv[2];
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    const completed = {};
-    const tasks = JSON.parse(body);
-    for (const i in tasks) {
-      const task = tasks[i];
-      if (task.completed === true) {
-        if (completed[task.userId] === undefined) {
-          completed[task.userId] = 1;
+// API URL
+const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+
+// Function to compute the number of completed tasks by user id
+function computeCompletedTasksByUserId (apiUrl) {
+  request(apiUrl, (error, response, body) => {
+    if (error) {
+      console.error('Error:', error);
+      return;
+    }
+
+    // Parse the response body
+    const todos = JSON.parse(body);
+
+    // Create an object to store the count of completed tasks for each user ID
+    const completedTasksByUserId = {};
+
+    // Loop through the todos and count completed tasks for each user ID
+    todos.forEach((todo) => {
+      if (todo.completed) {
+        if (completedTasksByUserId[todo.userId]) {
+          completedTasksByUserId[todo.userId]++;
         } else {
-          completed[task.userId]++;
+          completedTasksByUserId[todo.userId] = 1;
         }
       }
+    });
+
+    // Print users with completed tasks
+    console.log('Users with completed tasks:');
+    for (const userId in completedTasksByUserId) {
+      console.log(JSON.stringify(`${userId}:, ${completedTasksByUserId[userId]}`));
     }
-    console.log(completed);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
-  }
-});
+  });
+}
+
+// Call the function
+computeCompletedTasksByUserId(apiUrl);
