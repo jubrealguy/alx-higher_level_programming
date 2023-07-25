a64d24ff434e90fb6c09eda6f3c4efb8d027af12
jubrealguy <whizkidjubrealguy@gmail.com>
2023-07-25 13:35:26 +0100

    status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request.get(url, (err, res) => {
+  if (err) { console.log(err); } else {
+    console.log(`code: ${res.statusCode}`);
+  }
+});
